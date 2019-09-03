bebcbb1eabf6e75faf295b59aaffb1803a99e440
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-09-04 01:01:53 +0300

    test_del_project - список проектов через SOAP

--- test/test_del_project.py
+++ test/test_del_project.py
@@ -2,14 +2,14 @@ import random
 from model.project import Project
 
 
-def test_delete_project_by_name(app, db):
+def test_delete_project_by_name(app):
     app.session.login("administrator", "root")
     project = Project(name="Pr0", status="development", view_state="public", description="Some Project")
-    if db.get_project_list() == 0:
+    if app.soap.get_project_list() == 0:
         app.project.create(project)
-    old_projects = db.get_project_list()
+    old_projects = app.soap.get_project_list()
     project = random.choice(old_projects)
     app.project.delete_project_by_name(project.name)
-    new_projects = db.get_project_list()
+    new_projects = app.soap.get_project_list()
     old_projects.remove(project)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
