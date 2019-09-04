0f99ad5fa3eadcb34ba12ca1a0e7e2df42a291b5
Yaremenko Roman <roman.jaremenko@gmail.com>
2019-09-05 00:03:49 +0300

    добавлена уникальность имен проектов

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,9 +1,17 @@
 from model.project import Project
+import random
+import string
+
+
+def random_project_name(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
 def test_add_project(app):
     app.session.login("administrator", "root")
-    project = Project(name="Pr22", status="development", view_state="public", description="Some Project")
+    project_name = random_project_name("project_", 7)
+    project = Project(name=project_name , status="development", view_state="public", description="Some Project")
     old_projects = app.soap.get_project_list()
     app.project.create(project)
     new_projects = app.soap.get_project_list()
